387a56686fde908f45c04e92033e882f6b105ef2
Nathan Rzepecki <nathan@lionslair.net.au>
2022-01-21 15:48:44 +0800

    Minor uodates

--- src/Http/Controllers/XeroPayrollAuController.php
+++ src/Http/Controllers/XeroPayrollAuController.php
@@ -4,16 +4,13 @@ namespace Dcodegroup\LaravelXeroPayrollAu\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Dcodegroup\LaravelConfiguration\Models\Configuration;
+use Illuminate\Contracts\View\View;
 
 class XeroPayrollAuController extends Controller
 {
-    /**
-     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
-     */
-    public function __invoke()
+    public function __invoke(): View
     {
-        return view('xero-payroll-au-views::index', [
-            'configurations' => Configuration::byGroup('xero_payroll_au')->get(),
-        ]);
+        return view('xero-payroll-au-views::index')
+            ->with('configurations', Configuration::byGroup('xero_payroll_au')->get());
     }
 }

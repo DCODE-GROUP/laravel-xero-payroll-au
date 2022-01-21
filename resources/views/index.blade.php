15d4b049a9b966bc6d89cbb8fef7168a0f02642d
Nathan Rzepecki <nathan@lionslair.net.au>
2022-01-21 16:21:51 +0800

    Tweak the styling on the index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,7 @@
 @extends(config('laravel-xero-payroll-au.app_layout'))
 
 @section('content')
-    <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+    <div class="max-w-7xl mx-autos sm:px-6 lg:px-8">
         <div class="font-semibold text-xl text-gray-800 leading-tight">@lang('xero-payroll-au-translations::xero-payroll-au.heading')</div>
 
         <div>

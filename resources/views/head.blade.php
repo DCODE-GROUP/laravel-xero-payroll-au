96652b3fc5ee3694a4982331ff900e91913594dc
Nathan Rzepecki <nathan@lionslair.net.au>
2022-01-21 15:46:32 +0800

    Add new layouts to account for the header to be injected into pages

---
+++ resources/views/head.blade.php
@@ -0,0 +1,5 @@
+<!-- Fonts -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+
+<!-- Styles -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.11/tailwind.min.css">

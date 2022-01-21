bbcf68e491d892276c5e782b9bcf44deba25537f
Nathan Rzepecki <nathan@lionslair.net.au>
2022-01-21 15:48:32 +0800

    Add method to inject header into the given pages

--- src/LaravelXeroPayrollAuServiceProvider.php
+++ src/LaravelXeroPayrollAuServiceProvider.php
@@ -3,7 +3,11 @@
 namespace Dcodegroup\LaravelXeroPayrollAu;
 
 use Dcodegroup\LaravelXeroPayrollAu\Commands\InstallCommand;
+use Illuminate\Foundation\Http\Events\RequestHandled;
+use Illuminate\Http\Response as IlluminateResponse;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
 use XeroPHP\Application;
@@ -19,6 +23,7 @@ class LaravelXeroPayrollAuServiceProvider extends ServiceProvider
         $this->registerCommands();
         $this->registerResources();
         $this->registerRoutes();
+        $this->registerResponseHandler();
     }
 
     /**
@@ -69,5 +74,43 @@ class LaravelXeroPayrollAuServiceProvider extends ServiceProvider
                      ], function () {
                          $this->loadRoutesFrom(__DIR__ . '/../routes/xero_payroll_au.php');
                      });
+    }
+
+    /**
+     * Listen to the RequestHandled event to prepare the Response.
+     *
+     * @return void
+     */
+    private function registerResponseHandler()
+    {
+        Event::listen(RequestHandled::class, function (RequestHandled $event) {
+            if (! $event->request->ajax() &&
+                (($event->response->headers->has('Content-Type') && strpos($event->response->headers->get('Content-Type'), 'html') === true)
+                    || $event->request->getRequestFormat() == 'html'
+                    || stripos($event->response->headers->get('Content-Disposition'), 'attachment;') === false) &&
+                Str::startsWith($event->request->route()?->getName(), Str::slug(config('laravel-xero-payroll-au.path')).'.')) {
+                $content = $event->response->getContent();
+
+                $head = View::make('xero-payroll-au-views::head')->render();
+
+                // Try to put the js/css directly before the </head>
+                $pos = strripos($content, '</head>');
+                if (false !== $pos) {
+                    $content = substr($content, 0, $pos).$head.substr($content, $pos);
+                }
+
+                $original = null;
+                if ($event->response instanceof IlluminateResponse && $event->response->getOriginalContent()) {
+                    $original = $event->response->getOriginalContent();
+                }
+
+                $event->response->setContent($content);
+
+                // Restore original response (eg. the View or Ajax data)
+                if ($original) {
+                    $event->response->original = $original;
+                }
+            }
+        });
     }
 }
